2c426a9c7e46a8e345f204ee2fd1ccb5ed5d3222
Stephan Sun <stephansun@gmail.com>
2012-03-25 10:06:00 +0800

    add the missing fileupload controller class.

---
+++ samples-web/src/main/java/org/springframework/samples/mvc/fileupload/FileUploadController.java
@@ -0,0 +1,25 @@
+package org.springframework.samples.mvc.fileupload;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/fileupload")
+public class FileUploadController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public void fileUploadForm() {
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public void processUpload(@RequestParam MultipartFile file, Model model) throws IOException {
+		model.addAttribute("message", "File '" + file.getOriginalFilename() + "' uploaded successfully");
+	}
+	
+}
