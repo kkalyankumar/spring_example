24ce587a5527f7a6d9261444629440d4febc82de
Stephan Sun <stephansun@gmail.com>
2012-03-31 09:32:23 +0800

    add samples-web convert section.

---
+++ samples-web/src/main/java/org/springframework/samples/mvc/convert/SocialSecurityNumber.java
@@ -0,0 +1,25 @@
+package org.springframework.samples.mvc.convert;
+
+public final class SocialSecurityNumber {
+
+	private final String value;
+	
+	public SocialSecurityNumber(String value) {
+		this.value = value;
+	}
+	
+	@MaskFormat("###-##-####")
+	public String getValue() {
+		return value;
+	}
+
+	public static SocialSecurityNumber valueOf(@MaskFormat("###-##-####") String value) {
+		return new SocialSecurityNumber(value);
+	}
+
+	@Override
+	public String toString() {
+		return "SocialSecurityNumber [value=" + value + "]";
+	}
+	
+}
